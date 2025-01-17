3340edd4c9a8a2878f4e37f4df0d693d0a185685
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-01-18 01:19:56 +0600

    added work seets

--- index.js
+++ index.js
@@ -30,10 +30,33 @@ async function run() {
     await client.connect();
  
     const userCollection = client.db("employeeDB").collection("users");
+    const  sheetCollection = client.db("employeeDB").collection(" sheets");
 
 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({ token })
+    })
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization)
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
 
-
+// user
     app.post('/users', async (req, res) => {
         const user = req.body
   
@@ -45,6 +68,58 @@ async function run() {
         const result = await userCollection.insertOne(user)
         res.send(result)
       })
+      app.get('/users',verifyToken, async (req, res) => {
+        console.log(req.headers)
+        const result = await userCollection.find().toArray()
+        res.send(result)
+      })
+      app.get('/users/admin/:email',verifyToken, async (req, res) => {
+        const email = req.params.email
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: 'unauthorized access' })
+        }
+        const query = { email: email }
+        const user = await userCollection.findOne(query)
+        let admin = false
+        if (user) {
+          admin = user?.role === 'admin'
+        }
+        res.send({ admin })
+      })
+      app.get('/users/hr/:email',verifyToken, async (req, res) => {
+        const email = req.params.email
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: 'unauthorized access' })
+        }
+        const query = { email: email }
+        const user = await userCollection.findOne(query)
+        let hr = false
+        if (user) {
+          hr = user?.role === 'hr'
+        }
+        res.send({ hr })
+      })
+      //  Sheet
+      app.post('/sheets', async (req, res) => {
+        const item = req.body
+        const result = await sheetCollection.insertOne(item)
+        res.send(result)
+      })
+      app.get('/sheets', async (req, res) => {
+        const result = await sheetCollection.find().toArray()
+        res.send(result)
+      })
+      app.get('/sheets/:email', async (req, res) => {
+        const email = req.params.email;
+        // const decodedEmail=req.user?.email
+       
+        // if (decodedEmail !== email)
+        //   return res.status(401).send({ message: 'unauthorized access' })
+        const query =  {email }
+        const result = await sheetCollection.find(query).toArray();
+        res.send(result);
+      })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
