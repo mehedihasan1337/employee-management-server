dba865b311d7cff1452800cf5330ec5dbbfff94a
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-01-18 11:50:21 +0600

    added delete

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ucux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -120,6 +120,13 @@ async function run() {
         res.send(result);
       })
 
+   app.delete('/sheets/:id', async (req, res) => {
+  const id =req.params.id 
+  const query ={_id:new ObjectId(id)}
+  const result= await sheetCollection.deleteOne(query)
+  res.send(result)
+
+   })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
