2a9bf473a86ee177c0b630f2796053c9b4d2b5f7
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2025-01-19 00:14:10 +0600

    added patch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const app =express()
+const app = express()
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
 //  middleware
 app.use(cors())
@@ -28,9 +28,9 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
- 
+
     const userCollection = client.db("employeeDB").collection("users");
-    const  sheetCollection = client.db("employeeDB").collection(" sheets");
+    const sheetCollection = client.db("employeeDB").collection(" sheets");
 
 
     app.post('/jwt', async (req, res) => {
@@ -56,77 +56,108 @@ async function run() {
       })
     }
 
-// user
+    // user
     app.post('/users', async (req, res) => {
-        const user = req.body
-  
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query)
-        if (existingUser) {
-          return res.send({ message: 'user already exists', insertedId: null })
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-      })
-      app.get('/users',verifyToken, async (req, res) => {
-        console.log(req.headers)
-        const result = await userCollection.find().toArray()
-        res.send(result)
-      })
-      app.get('/users/admin/:email',verifyToken, async (req, res) => {
-        const email = req.params.email
-        if (email !== req.decoded.email) {
-          return res.status(403).send({ message: 'unauthorized access' })
-        }
-        const query = { email: email }
-        const user = await userCollection.findOne(query)
-        let admin = false
-        if (user) {
-          admin = user?.role === 'admin'
-        }
-        res.send({ admin })
-      })
-      app.get('/users/hr/:email',verifyToken, async (req, res) => {
-        const email = req.params.email
-        if (email !== req.decoded.email) {
-          return res.status(403).send({ message: 'unauthorized access' })
-        }
-        const query = { email: email }
-        const user = await userCollection.findOne(query)
-        let hr = false
-        if (user) {
-          hr = user?.role === 'hr'
-        }
-        res.send({ hr })
-      })
-      //  Sheet
-      app.post('/sheets', async (req, res) => {
-        const item = req.body
-        const result = await sheetCollection.insertOne(item)
-        res.send(result)
-      })
-      app.get('/sheets', async (req, res) => {
-        const result = await sheetCollection.find().toArray()
-        res.send(result)
-      })
-      app.get('/sheets/:email', async (req, res) => {
-        const email = req.params.email;
-        // const decodedEmail=req.user?.email
-       
-        // if (decodedEmail !== email)
-        //   return res.status(401).send({ message: 'unauthorized access' })
-        const query =  {email }
-        const result = await sheetCollection.find(query).toArray();
-        res.send(result);
-      })
+      const user = req.body
 
-   app.delete('/sheets/:id', async (req, res) => {
-  const id =req.params.id 
-  const query ={_id:new ObjectId(id)}
-  const result= await sheetCollection.deleteOne(query)
-  res.send(result)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+    app.get('/users', verifyToken, async (req, res) => {
+      console.log(req.headers)
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      let admin = false
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+    app.get('/users/hr/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      let hr = false
+      if (user) {
+        hr = user?.role === 'hr'
+      }
+      res.send({ hr })
+    })
+    //  Sheet
+    app.post('/sheets', async (req, res) => {
+      const item = req.body
+      const result = await sheetCollection.insertOne(item)
+      res.send(result)
+    })
+    app.get('/sheets', async (req, res) => {
+      const result = await sheetCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/sheets/single/:id', async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      if (!/^[a-fA-F0-9]{24}$/.test(id)) {
+        return res.status(400).json({ error: "Invalid ObjectId format" });
+    }
+      const query = { _id: new ObjectId(id) }
+      const result = await sheetCollection.findOne(query)
+      res.send(result)
 
-   })
+    })
+    app.patch('/sheets/update/:id', async (req, res) => {
+      const sheet = req.body
+      const id = req.params.id
+      if (!/^[a-fA-F0-9]{24}$/.test(id)) {
+        return res.status(400).json({ error: "Invalid ObjectId format" });
+    }
+      const filter = { _id: new ObjectId(id)  }
+      const updateDoc = {
+        $set: {
+            tasks: sheet.tasks,
+            hours: sheet.hours,
+            date: sheet.date,
+            // email: sheet.user.email
+
+        }
+      }
+      const result = await sheetCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    app.get('/sheets/:email', async (req, res) => {
+      const email = req.params.email;
+      // const decodedEmail=req.user?.email
+
+      // if (decodedEmail !== email)
+      //   return res.status(401).send({ message: 'unauthorized access' })
+      const query = { email }
+      const result = await sheetCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete('/sheets/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await sheetCollection.deleteOne(query)
+      res.send(result)
+
+    })
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -139,10 +170,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req, res)=>{
-    res.send('Employee Management server is running')
+app.get('/', (req, res) => {
+  res.send('Employee Management server is running')
 })
 
-app.listen(port,()=>{
-    console.log(`Employee Management server is running on port:${port}`)
+app.listen(port, () => {
+  console.log(`Employee Management server is running on port:${port}`)
 })
